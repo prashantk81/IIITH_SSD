a47e636fd57fe9ff1cbb89dbbb8f5f2e57dd1390
Prashant Kumar <prashantkumarr81@gmail.com>
2022-08-18 16:56:52 +0530

    final submission

--- ssd_lab_activity_3/2022201058_Q2.sql
+++ ssd_lab_activity_3/2022201058_Q2.sql
@@ -7,4 +7,4 @@ from EMPLOYEE
 group by Super_ssn) as temp
 where temp.Super_ssn= dp.Mgr_ssn) as temp2
 where temp2.Mgr_ssn= emp.Ssn
-
+order by temp2.ssn_count
--- ssd_lab_activity_3/2022201058_Q3.sql
+++ ssd_lab_activity_3/2022201058_Q3.sql
@@ -1,3 +1,6 @@
-select Mgr_ssn, Pnumber
-from DEPARTMENT as dp, PROJECT as pj
-where Pname='ProductY' and Dnumber=Dnum;
+select Mgr_ssn, count(*)
+from WORKS_ON as wt,
+(select Mgr_ssn
+from PROJECT as pj, DEPARTMENT as dp
+where Pname='ProductY' and pj.Dnum=dp.Dnumber) as temp
+where wt.Essn=temp.Mgr_ssn
